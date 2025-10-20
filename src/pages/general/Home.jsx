8fde479fbef0331557eaa882c8b0fb47239137e6
Muhammad Moeen <mooenmalik374@gmail.com>
2025-10-20 09:38:01 -0700

    FrontEnd Seperation

---
+++ src/pages/general/Home.jsx
@@ -0,0 +1,12 @@
+
+import VideoFeed from '../../components/VideoFeed';
+import '../../styles/videoFeed.css';
+const Home = () => {
+  return (
+    <div className="home-container">
+      <VideoFeed />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import UserRegister from "../pages/auth/UserRegister";
+import UserLogin from "../pages/auth/UserLogin";
+import PartnerLogin from "../pages/auth/PartnerLogin";
+import PartnerRegister from "../pages/auth/PartnerRegister";
+import Home from "../pages/general/Home";
+import CreateFood from "../pages/food-partener/CreateFood";
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/user/register" element={<UserRegister />} />
+        <Route path="/user/login" element={<UserLogin />} />
+        <Route path="/foodpartener/login" element={<PartnerLogin />} />
+        <Route path="/foodpartener/register" element={<PartnerRegister />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/createfood" element={<CreateFood/>}/>
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
